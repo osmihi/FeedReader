6018353816e195e06c49b20ddfaa258e04a67a76
Othman <othman@osmihi.com>
2012-04-26 16:14:39 -0500

    this is to test code as java application

---
+++ src/com/osmihi/feedReader/SiteTest.java
@@ -0,0 +1,11 @@
+package com.osmihi.feedReader;
+
+public class SiteTest {
+	
+	public static void main(String[] args) {
+		String msg = Site.makeSite("movie");
+		System.out.println(msg);
+	}
+	
+
+}
