5ce96422ecf7e1c53f5a424a5f89dc197e414aba
tippenein <tippenein@gmail.com>
2012-05-21 13:32:21 -0500

    Implement main_menu in MainActivity can select SearchActivity from the 'add new feed' button in the menu

--- src/com/osmihi/feedReader/MainActivity.java
+++ src/com/osmihi/feedReader/MainActivity.java
@@ -1,7 +1,11 @@
 package com.osmihi.feedReader;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -11,6 +15,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity{
 	
 	TextView labelMain;
+	Menu main_menu;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,4 +25,30 @@ public class MainActivity extends Activity{
         labelMain = (TextView)findViewById(R.id.labelMain);
         
     }
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_menu, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        
+	    	case R.id.searchFeed:
+	        	Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+	            this.startActivity(searchIntent);
+	            return true;
+	            
+	        case R.id.app_info:
+	            //showHelp();
+	        	//TODO make a print out of info option here
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
 }
